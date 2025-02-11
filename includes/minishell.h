801515f9c420564fc4b279f97c4d7b4abd5df0e6
Malo De Chateauvieux <malde-ch@cbr5s5.42barcelona.com>
2025-02-11 10:57:44 +0100

    removed struct envp

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: malde-ch <malo@chato.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:00:22 by malde-ch          #+#    #+#             */
-/*   Updated: 2025/02/11 10:52:58 by malde-ch         ###   ########.fr       */
+/*   Updated: 2025/02/11 10:57:23 by malde-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,10 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-
-typedef struct s_env
-{
-    char            *key;
-    char            *value;
-    struct s_env    *next;
-} t_env;
+//don't need this structure we have acess to the function getenv()
 
 typedef struct s_mini
 {
-    t_env   *env;
     char **envp;
 } t_mini;
 
