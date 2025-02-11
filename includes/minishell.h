0b3f8c8f1695b31715a205210a0aa54f98cebfcc
Malo De Chateauvieux <malde-ch@cbr5s5.42barcelona.com>
2025-02-11 13:43:24 +0100

    nueva structura

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: malde-ch <malo@chato.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 17:00:22 by malde-ch          #+#    #+#             */
-/*   Updated: 2025/02/11 10:57:23 by malde-ch         ###   ########.fr       */
+/*   Updated: 2025/02/11 13:42:59 by malde-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,41 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-//don't need this structure we have acess to the function getenv()
+
+typedef enum e_operator
+{
+    OP_NONE, // 0
+    OP_AND, // &&
+    OP_OR,  // ||
+    OP_PIPE, // |
+    OP_REDIRECT_OUT, // >
+    OP_REDIRECT_OUT_APPEND, // >>
+    OP_REDIRECT_IN, // <
+	OP_HERE_DOC, // <<
+} t_operator;
+
+
+typedef struct s_redirection
+{
+    char *file;
+    t_operator type;
+} t_redirection;
+
+
+typedef struct s_cmd
+{
+	char **cmd;
+	t_redirection *input;
+	t_redirection *output;
+	struct s_cmd *next;
+} t_cmd;
 
 typedef struct s_mini
 {
     char **envp;
+	t_cmd *cmd;
 } t_mini;
 
+#include "exec.h"
 
 #endif
